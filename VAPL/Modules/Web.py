dc66a5ea25a0a5f40b2065221d9ae70e18e5283e
NotMEE12 <legoMineFan@gmail.com>
2022-12-28 18:38:07 +0100

    TTS!

--- VAPL/Modules/Web.py
+++ VAPL/Modules/Web.py
@@ -9,12 +9,5 @@ def html(text):
 	return
 
 
-def run_JS(JS):
-	"""PLEASE DON't USE '\n' IN LINES!"""
-	for line in JS.split('\n'):
-		print(f'<style onload=\"{JS}\"\\>')
-	return
-
-
 def tts(text):
-	run_JS()
+	print(f'<style onload=\'speak({text})\'')
